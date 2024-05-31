d684fcb6e89506f3336f8d25dab6315409e89773
Juan Guadalupe <145798554+JuanECK@users.noreply.github.com>
2024-05-30 18:04:44 -0600

    widget fecha y hora2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -8,22 +7,17 @@ import { DatePipe } from '@angular/common';
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  template: `<div>
-  <p>Today is {{today | date}}</p>
-  <p>Or if you prefer, {{today | date:'fullDate'}}</p>
-  <p>The time is {{today | date:'jmZ'}}</p>
-</div>`,
 })
 export class AppComponent implements OnInit {
 
-  // constructor(private datePipe:DatePipe){}
-
   // newDate?:Date = new Date();
   // today: number = Date.now();
-  hora:any;
-  
+  // hora:any;
+
+  // propiedades del componente
   title = 'Ingenieria2.0';
 
+  // funcion que retorna la fecha
   getNowDate() {
     var returnDate = "";
     var today = new Date();
@@ -44,28 +38,61 @@ export class AppComponent implements OnInit {
     return returnDate;
   }
 
+  // funcion que retorna la hora
   getHora(){
     let returnDate = "";
     let hora = new Date
+    // console.log(new Date)
+    // let hora = new Date("Thu May 30 2024 12:16:25 GMT-0600 (GMT-06:00")
     let h = hora.getHours();
     let m = hora.getMinutes();
     let s = hora.getSeconds();
     let ampm;
 
     if(h >= 12){
-      h = h -12;
-      ampm = "P.M.";
+      if(h == 12){
+        h = 12;
+      }else{
+        h = h -12;
+      }
+      if(h < 10){
+        returnDate +=`0${h}:`
+      }else{
+        returnDate +=`${h}:`
+      }
+      ampm = " P.M.";
     }else{
-        ampm = "A.M."
+      if(h < 10){
+        if(h == 0){
+          returnDate +=`12:`
+        }else{
+          returnDate +=`0${h}:`
+        }
+      }else{
+        returnDate +=`${h}:`
+      }
+      ampm = " A.M."
     };
 
-    if(h == 0){
-        h = 12;
-    }
-    returnDate +=h + ":" +m +":" + s + " " +ampm
+
+     if(m < 10){
+      returnDate += `0${m}:`
+     }else{
+      returnDate += `${m}:`
+     }
+
+     if(s < 10){
+      returnDate += `0${s}`
+     }else{
+      returnDate += `${s}`
+     }
+
+    // returnDate +=h + ":" +m +":" + s + " " +ampm
+    returnDate += ampm
     return returnDate;
   }
   
+  // metodo de inicializacion
   ngOnInit(){
     this.mostrarHora();
   }
